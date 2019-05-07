e3ed638cc16c7dad2e9a84a6af6c4f57204fdcb2
Jamin Wu <Jamin.Wu07@gmail.com>
2019-05-08 04:46:25 +1000

    Very very basic task prototype.

--- 03-psychophysics/detection.py
+++ 03-psychophysics/detection.py
@@ -6,7 +6,9 @@
 
 # Imports
 import numpy as np
-from psychopy import visual, core
+from psychopy import visual, core, gui, data, event
+from psychopy.sound.backend_pygame import SoundPygame
+from psychopy.tools.filetools import fromFile, toFile
 from scipy.misc import imresize
 from phosphenes import RegularGrid, safebound
 
@@ -53,18 +55,94 @@ class Stimulus:
 
 if __name__ == "__main__":
 
-    # Window
-    window = visual.Window()
 
-    # Stimulus
+    ############################################################################
+    #  SETUP
+    ############################################################################
+
+    # Expensive preparations
     grid = RegularGrid()
 
-    for i in range(100):
-        xpos, ypos = np.random.random(2)
+    # Experiment details
+    details = {
+        "date": data.getDateStr(),
+        "participant": "",
+    }
 
-        stimulus = Stimulus(xpos, ypos)
-        rendered = grid.render(stimulus.vector)
+    # Initial user dialog
+    dialog = gui.DlgFromDict(details, title='PROTOTYPE', fixed=['date'])
+    if dialog.OK:
+        datafile =  "./data/{}_{}_details.pickle".format(details["participant"], details["date"])
+        toFile(datafile, details)
+    else:
+        core.quit()
 
-        imstim = visual.ImageStim(window, image=rendered)
-        imstim.draw()
-        window.flip()
+    # Clocks
+    clockglobal = core.Clock()
+    clocktrial = core.Clock()
+
+    # Window
+    win = visual.Window([640,480])
+
+    # Mouse
+    mouse = event.Mouse(win=win)
+
+    ntrials = 2
+    nframes = 60
+
+    ############################################################################
+    # INITIAL
+    ############################################################################
+
+    # incentre = False
+    # while not incentre:
+    #     pos = mouse.getPos()
+    #     print(pos)
+    #     if 0 < abs(pos[0]) < 0.5 and 0 < abs(pos[1]) < 0.5:
+    #         break
+
+    ############################################################################
+    # START
+    ############################################################################
+
+    rate = 0.02
+
+    tone = SoundPygame(secs=0.1)
+
+    with open(datafile.replace("details", "data"), "w+") as outfile:
+        outfile.write("trial,frame,xpos,ypos,mousexpos,mouseypos\n")
+
+        for t in range(ntrials):
+
+            intro = visual.TextStim(win, pos=[0,-100],text='Click the center when ready.')
+            fixation = visual.ShapeStim(win, lineColor=-1, lineColorSpace='rgb', vertices="cross", fillColor=-1, size=0.2)
+
+            intro.draw()
+            fixation.draw()
+            win.flip()
+            event.waitKeys()
+
+            xpos, ypos = 0.5, 0.5
+
+            for i in range(nframes):
+
+                pos = mouse.getPos()
+                mypos = (pos[0] + 1) / 2
+                mxpos = (pos[1] + 1) / 2
+                datastring = "{},{},{},{},{},{}\n".format(t,i,xpos,ypos,mxpos,mypos)
+                if abs(xpos - mxpos) > 0.1 or abs(ypos - mypos) > 0.1:
+                    tone.play()
+                print(datastring)
+                outfile.write(datastring)
+
+                direction = np.random.uniform(2*np.pi)
+                xshift, yshift = rate*np.cos(direction), rate*np.sin(direction)
+                xpos += xshift
+                ypos += yshift
+
+                stimulus = Stimulus(xpos, ypos)
+                rendered = grid.render(stimulus.vector)
+
+                imstim = visual.ImageStim(win, image=rendered, size=(2,2))
+                imstim.draw()
+                win.flip()
