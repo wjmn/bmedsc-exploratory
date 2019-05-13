0033082dbd39a183217ba93e0d905d2a5834aa63
Jamin Wu <Jamin.Wu07@gmail.com>
2019-05-13 14:26:18 +1000

    Formatting with black.

--- 03-psychophysics/digits.py
+++ 03-psychophysics/digits.py
@@ -7,7 +7,8 @@
 # Imports
 import numpy as np
 from psychopy import visual, core, gui, data, event
-#from psychopy.sound.backend_pygame import SoundPygame
+
+# from psychopy.sound.backend_pygame import SoundPygame
 from psychopy.tools.filetools import fromFile, toFile
 from scipy.misc import imresize
 from skimage import color
@@ -29,11 +30,22 @@ EYSIZE = YSIZE // SCALE
 # STIMULUS
 ################################################################################
 
-datadir = 'data/digit-images/'
-dataext = '.png'
-imagesize = np.shape(imread(datadir+str(0)+dataext)) # assume all images the same size as image 0
-imagescale = EXSIZE / imagesize[0] # assuming a square image
-stimuli = [np.fliplr(imresize(color.rgb2gray(imread(datadir + str(digit) + dataext)).transpose(), imagescale)) for digit in range(10)]
+datadir = "data/digit-images/"
+dataext = ".png"
+imagesize = np.shape(
+    imread(datadir + str(0) + dataext)
+)  # assume all images the same size as image 0
+imagescale = EXSIZE / imagesize[0]  # assuming a square image
+stimuli = [
+    np.fliplr(
+        imresize(
+            color.rgb2gray(imread(datadir + str(digit) + dataext)).transpose(),
+            imagescale,
+        )
+    )
+    for digit in range(10)
+]
+
 
 class Stimulus:
     def __init__(self, digit):
@@ -43,7 +55,7 @@ class Stimulus:
     def process(self):
         """ Converts the stimulus into a brightness vector for the
         """
-        flattened = self.image.flatten(order='F')
+        flattened = self.image.flatten(order="F")
         return flattened
 
 
@@ -61,15 +73,14 @@ if __name__ == "__main__":
     grid = RegularGrid(exsize=EXSIZE, eysize=EYSIZE)
 
     # Experiment details
-    details = {
-        "date": data.getDateStr(),
-        "participant": "",
-    }
+    details = {"date": data.getDateStr(), "participant": ""}
 
     # Initial user dialog
-    dialog = gui.DlgFromDict(details, title='PROTOTYPE', fixed=['date'])
+    dialog = gui.DlgFromDict(details, title="PROTOTYPE", fixed=["date"])
     if dialog.OK:
-        datafile =  "./data/{}_{}_details.pickle".format(details["participant"], details["date"])
+        datafile = "./data/{}_{}_details.pickle".format(
+            details["participant"], details["date"]
+        )
         toFile(datafile, details)
     else:
         core.quit()
@@ -80,10 +91,10 @@ if __name__ == "__main__":
     clocktrial = core.Clock()
 
     # Window
-    win = visual.Window([XSIZE,YSIZE])
+    win = visual.Window([XSIZE, YSIZE])
 
     ntrials = 30
-    ncues   = 20
+    ncues = 20
 
     for trial in range(ntrials):
 
@@ -93,7 +104,7 @@ if __name__ == "__main__":
             stimulus = Stimulus(digit)
             rendered = grid.render(stimulus.vector)
 
-            imstim = visual.ImageStim(win, image=rendered, size=(2,2))
+            imstim = visual.ImageStim(win, image=rendered, size=(2, 2))
             imstim.draw()
             win.flip()
             event.waitKeys()
--- 03-psychophysics/phosphenes.py
+++ 03-psychophysics/phosphenes.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 from scipy.ndimage import gaussian_filter
-#from scipy.ndimages.filters import convolve
+
+# from scipy.ndimages.filters import convolve
 
 # CONSTANTS
 
@@ -13,38 +14,44 @@ SCALE = 80
 EXSIZE = XSIZE // SCALE
 EYSIZE = YSIZE // SCALE
 
+
 def safebound(value, width, lower, upper):
-    vmin = int(max(lower, value-width))
-    vmax = int(min(upper, value+width))
+    vmin = int(max(lower, value - width))
+    vmax = int(min(upper, value + width))
     return vmin, vmax
+
 
 class Electrode:
     def __init__(self, x, y):
-        #self.phosphene = phosphene
+        # self.phosphene = phosphene
         self.x = x
         self.y = y
-        self.size = PBASE * (0.5 + (4 * np.sqrt((x-0.5)**2 + (y-0.5)**2))**2)
+        self.size = PBASE * (0.5 + (4 * np.sqrt((x - 0.5) ** 2 + (y - 0.5) ** 2)) ** 2)
 
         self.rendered = self.render()
 
     def render(self, xsize=YSIZE, ysize=XSIZE):
-        xmin, xmax = safebound(XSIZE*self.x, self.size, 0, XSIZE)
-        ymin, ymax = safebound(YSIZE*self.y, self.size, 0, YSIZE)
+        xmin, xmax = safebound(XSIZE * self.x, self.size, 0, XSIZE)
+        ymin, ymax = safebound(YSIZE * self.y, self.size, 0, YSIZE)
 
-        base = np.zeros((ysize,xsize))
+        base = np.zeros((ysize, xsize))
         base[ymin:ymax, xmin:xmax] = 1
 
         return gaussian_filter(base, self.size)
+
 
 class RegularGrid:
     def __init__(self, exsize=EXSIZE, eysize=EYSIZE):
         self.exsize = EXSIZE
         self.eysize = EYSIZE
-        self.grid = [Electrode(y/eysize,x/exsize)
-                     for x in range(exsize)
-                     for y in range(eysize)]
+        self.grid = [
+            Electrode(y / eysize, x / exsize)
+            for x in range(exsize)
+            for y in range(eysize)
+        ]
+
     def render(self, values):
-        product = [v * e.rendered for (v,e) in zip(values, self.grid)]
+        product = [v * e.rendered for (v, e) in zip(values, self.grid)]
         summed = sum(product)
         summax = np.max(summed)
         return (summed / summax) * 2 - 1
