bdd441a18d18b626f8816969bcbac31012153101
wjmn <Jamin.Wu07@gmail.com>
2019-08-01 11:12:16 +1000

    Whitespace clean, fixed image_filepath.

--- 04-gan/train.py
+++ 04-gan/train.py
@@ -9,12 +9,12 @@ import PIL
 import pickle
 from datetime import datetime
 from tensorflow.keras import layers, Sequential, Model
-from tensorflow.keras.layers import * 
+from tensorflow.keras.layers import *
 from scipy.ndimage import gaussian_filter
 from grid import PolarGrid, CartesianGrid, render, render_tensor
 
 ################################################################################
-# GRID 
+# GRID
 ################################################################################
 
 gridType = PolarGrid
@@ -48,9 +48,9 @@ def process_images(images):
             # Pad and resize up to the render dimensions
             np.pad(
                 cv2.resize(
-                    image, 
+                    image,
                     dsize=(up_y, up_x)
-                ), 
+                ),
                 pad_widths,
                 'constant',
                 constant_values=0
@@ -77,29 +77,29 @@ filler_path = "../03-psychophysics/data/training-hole-filling/training-encoders/
 filler = tf.keras.models.load_model(filler_path)
 
 ################################################################################
-# DEFINING MODEL 
+# DEFINING MODEL
 ################################################################################
 
 # Defining an encoder
 SEED_SIZE = 32
 def make_encoder():
-    
+
     # Input noise
     in_noise        = Input(shape=(SEED_SIZE,))
-    
+
     # Layers for noise
     l_noise         = BatchNormalization()(in_noise)
     l_noise         = LeakyReLU()(l_noise)
-    
+
     # Input labels
     in_labels       = Input(shape=(1,))
-    
+
     # Layers for labels
     l_labels        = Embedding(10, gridVectorLength // 4)(in_labels)
     l_labels        = Flatten()(l_labels)
     l_labels        = BatchNormalization()(l_labels)
     l_labels        = LeakyReLU()(l_labels)
-    
+
     # Concatenate
     concat          = Concatenate()([l_noise, l_labels])
     concat          = Flatten()(concat)
@@ -110,44 +110,44 @@ def make_encoder():
     concat          = LeakyReLU()(concat)
     concat          = Concatenate()([concat, l_labels])
     concat          = Dense(gridVectorLength, activation=tf.nn.sigmoid)(concat)
-    
+
     # Model
     model           = Model([in_noise, in_labels], concat)
-    
+
     return model
 
 # Defining a decoder
 def make_decoder():
-    
+
     # Image input
     in_images     = Input(shape=(renderSize1, renderSize2))
     l_images      = Reshape((renderSize1, renderSize2, 1))(in_images)
-    
+
     # Convolution
     concat        = Conv2D(64, (8,8), padding='same', strides=(2,2))(l_images)
     concat        = LeakyReLU()(concat)
     concat        = Dropout(0.25)(concat)
-    
+
     label_out     = Conv2D(128, (4,4), padding='same', strides=(2,2))(concat)
     label_out     = LeakyReLU()(label_out)
     label_out     = Dropout(0.25)(label_out)
     label_out     = Flatten()(label_out)
     label_out     = Dense(11, activation=tf.nn.softmax)(label_out)
-    
-    # Model 
+
+    # Model
     model         = Model(inputs=[in_images], outputs=label_out)
-    
-    return model    
+
+    return model
 
 # Convenience functions
 @tf.function
 def make_inputs(labels):
-    
+
     batch_size = len(labels)
-    
+
     noise = tf.random.uniform((batch_size, SEED_SIZE))
     one_hot = tf.one_hot(labels, depth=10, dtype=tf.float32)
-    
+
     return (noise, one_hot)
 
 # Loss functions
@@ -155,26 +155,26 @@ binary_cross_entropy = tf.keras.losses.BinaryCrossentropy()
 categorical_cross_entropy = tf.keras.losses.CategoricalCrossentropy()
 
 def encoder_loss(real_labels, decoded_one_hot, renders):
-    
+
     real_one_hot = tf.one_hot(real_labels, depth=11)
     label_loss = categorical_cross_entropy(real_one_hot, decoded_one_hot)
-    
+
     brightness_loss = tf.reduce_sum(tf.math.cos(tf.constant(np.pi / 2) * renders))
-    
+
     return label_loss + (brightness_loss / (renderSize1 * renderSize2 * 100))
 
 def decoder_loss(real_labels, decoded_real_one_hot, decoded_fake_one_hot):
-    
+
     shape = real_labels.shape
-        
+
     real_one_hot = tf.one_hot(real_labels, depth=11)
     real_loss = categorical_cross_entropy(real_one_hot, decoded_real_one_hot)
-    
+
     all_fake_one_hot = tf.one_hot(tf.fill(shape, 10), depth=11)
     fake_loss = categorical_cross_entropy(all_fake_one_hot, decoded_fake_one_hot)
-    
+
     loss = real_loss + fake_loss
-    
+
     return loss
 
 # Optimizers
@@ -255,24 +255,24 @@ curried_render = lambda x: render_tensor(grid, x)
 
 @tf.function
 def train_step(real_images, real_labels):
-    
+
     with tf.GradientTape() as enc_tape, tf.GradientTape() as dec_tape:
-        
+
         # Generate the encoder inputs with a random seed
         seeds, one_hot_labels = make_inputs(real_labels)
-        
+
         # Produce encodings from the encoder inputs
         encodings = encoder((seeds, real_labels), training=True)
 
         # Render the encodings
         encoded_renders = tf.map_fn(curried_render, encodings)
-        
+
         # Fill holes in encoded renders
         filled_renders = filler(encoded_renders)
-        
+
         # Decode the rendered images
         decoded_fake_one_hot = decoder((filled_renders,))
-        
+
         # Decode the real images
         decoded_real_one_hot = decoder((real_images,))
 
@@ -283,14 +283,14 @@ def train_step(real_images, real_labels):
     # Output the losses to the log files
     tf.print(enc_loss, output_stream=enc_logfile)
     tf.print(dec_loss, output_stream=dec_logfile)
-    
+
     # Calculate and apply the gradients to the encoder and decoder
     gradients_of_encoder = enc_tape.gradient(enc_loss, encoder.trainable_variables)
     gradients_of_decoder = dec_tape.gradient(dec_loss, decoder.trainable_variables)
 
     encoder_optimizer.apply_gradients(zip(gradients_of_encoder, encoder.trainable_variables))
     decoder_optimizer.apply_gradients(zip(gradients_of_decoder, decoder.trainable_variables))
-    
+
 ################################################################################
 # Training display
 ################################################################################
@@ -299,11 +299,11 @@ display_labels = tf.convert_to_tensor(range(10))
 display_seeds, display_one_hot = make_inputs(display_labels)
 
 def generate_and_save_images(encoder, epoch):
-    
+
     encodings = encoder((display_seeds, display_labels), training=False)
-    
+
     fig, axes = plt.subplots(1, 10, figsize=(10, 1))
-    
+
     for i in range(10):
         axes[i].imshow(curried_render(encodings[i].numpy()), cmap='gray', vmin=-1, vmax=1)
         axes[i].set_title(i)
@@ -314,7 +314,7 @@ def generate_and_save_images(encoder, epoch):
     plt.savefig(image_filepath.replace('.png', '_epoch-{0:02d}.png'.format(epoch)))
     plt.tight_layout()
     plt.close()
-    
+
 generate_and_save_images(encoder, 0)
 
 ################################################################################
@@ -341,19 +341,19 @@ with open(enc_logpath, 'w') as outfile:
     encoder_loss_value = encoder_loss(initial_labels, initial_decoded_fake, initial_renders)
     outfile.write(str(encoder_loss_value.numpy()))
     outfile.write('\n')
-    
+
 with open(dec_logpath, 'w') as outfile:
     decoder_loss_value = decoder_loss(initial_labels, initial_decoded_real, initial_decoded_fake)
     outfile.write(str(decoder_loss_value.numpy()))
     outfile.write('\n')
-    
-    
+
+
 ################################################################################
 # Defining the training loop
 ################################################################################
 
 def train(epochs):
-    
+
     for epoch in range(epochs):
         start = time.time()
         print(f'Starting epoch {epoch+1} at {time.asctime()}')
@@ -361,16 +361,16 @@ def train(epochs):
         for i in range(NUM_BATCHES):
             imin = i * BATCH_SIZE
             imax = (i+1) * BATCH_SIZE
-            
+
             real_images_slice = tf.cast(train_images[imin:imax], tf.float32)
             real_labels_slice = tf.cast(train_labels[imin:imax], tf.int32)
-            
+
             train_step(real_images_slice, real_labels_slice)
 
         generate_and_save_images(encoder, epoch + 1)
 
         print(f'Time for epoch {epoch+1} is {time.time()-start} sec.')
-        
+
 ################################################################################
 # Train
 ################################################################################
@@ -384,7 +384,7 @@ train(EPOCHS)
 ################################################################################
 
 # Generate GIF
-generated_images = glob.glob(image_directory + (f"{now}_*.png"))
+generated_images = glob.glob(image_filepath.replace(".png", "*.png"))
 images = [PIL.Image.open(image) for image in generated_images]
 
 # save the first image 10 times
